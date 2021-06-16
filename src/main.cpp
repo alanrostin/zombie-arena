72c40c92cdc2d14c2470a1c9fa9d80247fe16134
alanrostin <alan.rostin@gmail.com>
2021-06-16 11:33:36 +0430

    Add player and zombie collision detection

--- src/main.cpp
+++ src/main.cpp
@@ -384,6 +384,23 @@ int main()
                     }
                 }
             }
+
+            // Have any zombies touched the player
+            for (int i = 0; i < numZombies; i++)
+            {
+                if (player.getPosition().intersects(zombies[i].getPosition()) && zombies[i].isAlive())
+                {
+                    if (player.hit(gameTimeTotal))
+                    {
+
+                    }
+
+                    if (player.getHealth() <= 0)
+                    {
+                        state = State::GAME_OVER;
+                    }
+                }
+            }
         }
 
         /*
